8d582371f4f2ebaf20f665bad5ddb3c746041e7a
Dae hyeon Mun <daehyeonmundev@gmail.com>
2023-09-03 22:52:07 +0900

    feat: add reanimated, skia, gesture handler and lodash

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import {Text, View} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      <Text>App</Text>
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <Text>App</Text>
+      </View>
+    </GestureHandlerRootView>
   );
 };
