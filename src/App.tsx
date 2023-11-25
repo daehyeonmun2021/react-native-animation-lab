67cfaf448f10d774b5a2b88fce6ace8a8457d21a
Dae hyeon Mun <daehyeonmundev@gmail.com>
2023-11-26 00:44:54 +0900

    Hide StatusBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { RootStack } from './screens';
+import { StatusBar } from 'react-native';
 
 export const App = () => {
   return (
     <NavigationContainer>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <RootStack />
+        <>
+          <StatusBar hidden />
+          <RootStack />
+        </>
       </GestureHandlerRootView>
     </NavigationContainer>
   );
